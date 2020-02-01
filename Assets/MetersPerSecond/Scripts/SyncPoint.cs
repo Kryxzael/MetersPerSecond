8ebc2a4a0be7def1c4d650df6c900f8955ca7263
Kryxzael <kasper.ramskjell98@gmail.com>
2020-02-01 07:15:12 +0100

    Implement the basics of sync points

---
+++ Assets/MetersPerSecond/Scripts/SyncPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.MetersPerSecond.Scripts
+{
+    public class SyncPoint : Track
+    {
+        protected override void OnObjectEnter(TrackObject obj)
+            => Debug.Log("Sync detected");
+    }
+}
